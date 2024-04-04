d7e309f9c2db49aa79d81a62dbf20fb8bf0a9b49
Ace-16 <samdeshmukh300@gmail.com>
2024-04-04 13:33:44 +0530

    Slider-updated

--- src/components/Imgslider.jsx
+++ src/components/Imgslider.jsx
@@ -13,22 +13,22 @@ function Imgslider() {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoPlay: true
+    autoplay: true
   };
   return (
     <Carousel {...settings}>
-        <Wrap>
-          <img src="/images/slider-badag.jpg" alt="" />
-        </Wrap> 
-        <Wrap>
-          <img src="/images/slider-badging.jpg" alt="" />
-        </Wrap> 
-        <Wrap>
-          <img src="/images/slider-scale.jpg" alt="" />
-        </Wrap> 
-        <Wrap>
-          <img src="/images/slider-scales.jpg" alt="" />
-        </Wrap>
+      <Wrap>
+        <img src="/images/slider-badag.jpg" alt="" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/slider-badging.jpg" alt="" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/slider-scale.jpg" alt="" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/slider-scales.jpg" alt="" />
+      </Wrap>
     </Carousel>
   )
 }
@@ -36,9 +36,42 @@ function Imgslider() {
 export default Imgslider
 
 const Carousel = styled(Slider)`
+  margin-top: 20px;
+  
 
+  ul li button {
+    &: before{
+      font-size: 10px;
+      color: rgb(150, 158, 171);
+    }
+  }
+
+  li.slick-active button::before {
+    color: white;
+  }
+
+  .slick-list {
+    overflow: visible;
+  }
+
+  button {
+    z-index: 1;
+  }
 `
 
 const Wrap = styled.div`
+  cursor: pointer;
+  img {
+    border: 4px solid transparent;
+    border-radius: 4px;
+    width: 100%;
+    height: 100%;
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px  30px -10px,
+    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    transition-duration: 300ms;
 
+    &:hover {
+      border: 4px solid rgb(249, 249, 249, 0.8);
+    }
+  }
 `
